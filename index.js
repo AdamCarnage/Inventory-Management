c1f8fc488c87efa2de7fe80435ba3ce2b3cd522d
AdamCarnage <104533419+AdamCarnage@users.noreply.github.com>
2023-12-01 23:55:22 +0300

    updated functionalities on sales and home page

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const methodOverride = require("method-override");
 const { Pool } = require("pg");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "css")));
+app.use("/css", express.static("public/css"));
+app.use(cookieParser());
+
+
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false, maxAge: 1200000 }
+}));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -19,19 +31,35 @@ const pool = new Pool({
   port: 8080,
 });
 
-app.get("/", (req, res) => {
-  res.render("home.ejs");
-});
+const requireLogin = (req, res, next) => {
+  if (req.session && req.session.userId) {
+    return next();
+  } else {
+    return res.redirect('/');
+  }
+};
 
-app.get("/loged-in", (req, res) => {
-  res.render("loged-in");
+app.get("/", (req, res) => {
+  res.render("login.ejs");
 });
 
 app.get("/register", (req, res) => {
   res.render("regin.ejs");
 });
 
-app.post("/home", (req, res) => {
+app.get("/testrun", (req, res) => {
+  res.render("test.ejs");
+});
+
+app.get("/barcode", (req, res) => {
+  res.render("barcode.ejs");
+});
+
+app.get("/customerservice", (req, res) => {
+  res.render("customer.ejs");
+});
+
+app.post("/", (req, res) => {
   const { email, name, pass } = req.body;
 
   const checkUserQuery =
@@ -44,9 +72,10 @@ app.post("/home", (req, res) => {
       res.send("Error occurred during login.");
     } else {
       if (result.rows.length > 0) {
+        req.session.user = { name, email };
         res.send(` <script>
                 alert('Welcome, ${name}!');
-                window.location.href = '/logedIn'; 
+                window.location.href = '/home'; 
                 </script>`);
       } else {
         res.send(`<script>
@@ -58,7 +87,7 @@ app.post("/home", (req, res) => {
   });
 });
 
-app.post("/regin", (req, res) => {
+app.post("/register", (req, res) => {
   const { name, f_name, l_name, email, pass } = req.body;
 
   const insertQuery =
@@ -73,14 +102,24 @@ app.post("/regin", (req, res) => {
       );
     } else {
       res.send(
-        '<script>alert("Your successful registered as new Admin"); window.location.href = "/home"; </script>'
+        '<script>alert("Your successful registered as new Admin"); window.location.href = "/"; </script>'
       );
     }
   });
 });
 
-app.post("/loged-in", (req, res) => {
+
+app.post("/home",requireLogin, (req, res) => {
   const { item, Qty, category, cost, exp, id } = req.body;
+
+  const expirationDate = new Date(exp);
+
+  if (expirationDate <= new Date()) {
+    res.send(
+      '<script>alert("Expiration date has already passed. Please enter a valid date."); window.location.href = "/home"; </script>'
+    );
+    return;
+  }
 
   const insertQuery =
     "INSERT INTO new_items (item_name, item_qty, item_category, item_price, exp_date, id) VALUES ($1, $2, $3, $4, $5, $6)";
@@ -90,25 +129,31 @@ app.post("/loged-in", (req, res) => {
     if (error) {
       console.error("Error executing query", error);
       res.send(
-        '<script>alert("There was an error in submitting"); window.location.href = "/loged-in"; </script>'
+        '<script>alert("There was an error in submitting"); window.location.href = "/home"; </script>'
       );
     } else {
-      res.redirect("/logedIn");
+      res.redirect("/home");
     }
   });
 });
 
-app.get("/logedIn", (req, res) => {
-  pool.query("SELECT * FROM new_items", (error, result) => {
-    if (error) {
-      console.error("Error executing query", error);
-      res.status(500).send("Error occurred while fetching data.");
+
+app.get("/home", async (req, res) => {
+  try{
+   const itemdata = await pool.query("SELECT * FROM new_items");
+   const data = itemdata.rows;
+   res.render("home.ejs", { data });
+  }
+  catch (error){
+    console.error("Error executing query", error);
+    if (process.env.NODE_ENV === 'development') {
+        res.status(500).send(`Error occurred while fetching data: ${error.message}`);
     } else {
-      const data = result.rows;
-      res.render("loged-in.ejs", { data: data });
+        res.status(500).send("Error occurred while fetching data.");
     }
+  }
   });
-});
+
 
 app.delete("/delete/:id", (req, res) => {
   const itemId = req.params.id;
@@ -127,9 +172,6 @@ app.delete("/delete/:id", (req, res) => {
   );
 });
 
-app.get("/home", (req, res) => {
-  res.redirect("/");
-});
 
 app.get("/edit/:id", (req, res) => {
   const itemId = req.params.id;
@@ -169,15 +211,211 @@ app.post("/edit/:id", (req, res) => {
     if (error) {
       console.error("Error executing query", error);
       res.send(
-        '<script>alert("There was an error in updating"); window.location.href = "/logedIn"; </script>'
+        '<script>alert("There was an error in updating"); window.location.href = "/home"; </script>'
       );
     } else {
       res.send(
-        '<script>alert("The product details were updated successfuly"); window.location.href = "/logedIn"; </script>'
+        '<script>alert("The product details were updated successfuly"); window.location.href = "/home"; </script>'
       );
     }
   });
 });
+
+
+app.post('/sale', (req, res) => {
+  const { item_id, item_name, Qty, category, cost } = req.body;
+
+  const getCostQuery = 'SELECT item_price FROM new_items WHERE id = $1';
+  const getCostValues = [item_id];
+
+  pool.query(getCostQuery, getCostValues, (error, result) => {
+    if (error) {
+      console.error('Error executing query', error);
+      res.status(500).send('Error occurred while fetching item cost.');
+    } else {
+      if (result.rows.length === 0) {
+        res.send('<script>alert("No item with such ID!!!"); window.location.href = "/sale"; </script>');
+        return;
+      }
+      const itemCost = result.rows[0].item_price;
+
+      if (itemCost !== cost) {
+        res.send('<script>alert("The provided price does not match the item\'s price."); window.location.href = "/sale"; </script>');
+        return;
+      }
+
+      const getQtyQuery = 'SELECT item_qty FROM new_items WHERE id = $1';
+      const getQtyValues = [item_id];
+
+      pool.query(getQtyQuery, getQtyValues, (error, result) => {
+        if (error) {
+          console.error('Error executing query', error);
+          res.status(500).send('Error occurred while fetching item quantity.');
+        } else {
+          if (result.rows.length === 0) {
+            res.send('<script>alert("No item with such ID!!!"); window.location.href = "/sale"; </script>');
+            return;
+          }
+          const currentQty = result.rows[0].item_qty;
+          
+          if (currentQty >= Qty) {
+            // Calculate total cost
+            const total_price = Qty * cost;
+
+            // Set the current date and time
+            const currentDate = new Date().toISOString();
+
+            const insertSalesQuery = 'INSERT INTO sales (id, item_name, item_qty, item_category, item_price, total_price, sale_date) VALUES ($1, $2, $3, $4, $5, $6, $7)';
+            const salesValues = [item_id, item_name, Qty, category, cost, total_price, currentDate];
+
+            pool.query(insertSalesQuery, salesValues, (error, result) => {
+              if (error) {
+                console.error('Error executing query', error);
+                res.status(500).send('Error occurred while registering sale.');
+              } else {
+                const updateQtyQuery = 'UPDATE new_items SET item_qty = item_qty - $1 WHERE id = $2';
+                const updateValues = [Qty, item_id];
+
+                pool.query(updateQtyQuery, updateValues, (error, result) => {
+                  if (error) {
+                    console.error('Error executing query', error);
+                    res.status(500).send('Error occurred while updating quantity.');
+                  } else {
+                    console.log(currentQty);
+
+                    if (currentQty - Qty <= 0) {
+                      const removeItemQuery = 'DELETE FROM new_items WHERE id = $1';
+                      const removeItemValues = [item_id];
+
+                      pool.query(removeItemQuery, removeItemValues, (error, result) => {
+                        if (error) {
+                          console.error('Error executing query', error);
+                          res.status(500).send('Error occurred while removing item.');
+                        } else {
+                          res.send('<script>alert("The product sales were successful recorded!!!"); window.location.href = "/sale"; </script>');
+                        }
+                      });
+                    } else {
+                      res.send('<script>alert("The product sales were successful recorded!!!"); window.location.href = "/sale"; </script>');
+                    }
+                  }
+                });
+              }
+            });
+          } else {
+            res.status(400).send('<script>alert("The product sales were not recorded!!!"); window.location.href = "/sale"; </script>');
+          }
+        }
+      });
+    }
+  });
+});
+
+
+app.get('/sale', (req, res) => {
+
+  const sessionData = req.session;
+
+  const getSalesQuery = 'SELECT * FROM sales ORDER BY sale_date DESC'; // Assuming you have a sale_date column in your sales table
+  pool.query(getSalesQuery, (error, result) => {
+    if (error) {
+      console.error('Error executing query', error);
+      res.status(500).send('Error occurred while fetching sales details.');
+    } else {
+      const salesDetails = result.rows;
+      const groupedSales = {};
+
+      salesDetails.forEach(sale => {
+        const saleDate = sale.sale_date ? sale.sale_date.toISOString().split('T')[0] : 'N/A';
+        if (!groupedSales[saleDate]) {
+          groupedSales[saleDate] = [];
+        }
+        groupedSales[saleDate].push(sale);
+      });
+
+      res.render('sales.ejs', { groupedSales });
+    }
+  });
+});
+
+
+//----------------------------------------code for barcode----------------------------------//
+// const salesDatabase = [];
+
+// app.use(bodyParser.json());
+
+// app.post('/scan', (req, res) => {
+//   const { barcode, quantity } = req.body;
+//   const product = productsDatabase[barcode];
+
+//   if (product) {
+//     const totalPrice = product.price * quantity;
+
+//     // Register the sale
+//     const saleDetails = {
+//       barcode,
+//       productName: product.name,
+//       quantity,
+//       totalPrice,
+//       timestamp: new Date(),
+//     };
+
+//     salesDatabase.push(saleDetails);
+
+//     // Update inventory (in a real system, you might decrement the quantity)
+    
+//     res.json({ success: true, message: 'Sale registered successfully', saleDetails });
+//   } else {
+//     res.json({ success: false, message: 'Product not found' });
+//   }
+// });
+
+// app.get('/sales', (req, res) => {
+//   res.json(salesDatabase);
+// });
+
+//------------------------------------code for pass encryption---------------------------------//
+// const bcrypt = require('bcrypt');
+// const saltRounds = 10;
+
+// app.post('/api/register', async (req, res) => {
+//   const { firstName, lastName, email, password } = req.body;
+
+//   try {
+//     // Generate a random 5-digit customer ID
+//     const customerId = Math.floor(10000 + Math.random() * 90000);
+
+//     // Check if the email is already registered
+//     const checkEmailQuery = 'SELECT * FROM customer WHERE customer_email = $1';
+//     const checkEmailValues = [email];
+//     const emailResult = await pool.query(checkEmailQuery, checkEmailValues);
+
+//     if (emailResult.rows.length > 0) {
+//       return res.status(400).json({ error: 'Email already registered' });
+//     }
+
+//     // Hash the password
+//     bcrypt.hash(password, saltRounds, async (err, hash) => {
+//       if (err) {
+//         console.error('Error hashing password:', err);
+//         return res.status(500).json({ error: 'Internal Server Error' });
+//       }
+
+//       // If email is not registered, insert the new customer with the generated ID and hashed password
+//       const insertCustomerQuery =
+//         'INSERT INTO customer (id, first_name, second_name, customer_email, customer_password) VALUES ($1, $2, $3, $4, $5)';
+//       const insertCustomerValues = [customerId, firstName, lastName, email, hash];
+//       await pool.query(insertCustomerQuery, insertCustomerValues);
+
+//       res.status(201).json({ message: 'Registration successful!', customerId });
+//     });
+//   } catch (error) {
+//     console.error('Error registering customer:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
+
+
 
 app.listen(3000, (req, res) => {
   console.log("App on Port 3000 Active");
