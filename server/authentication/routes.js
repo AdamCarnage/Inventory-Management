9cbc2b4b7fc4fa82cbafe38f15b36348daee0561
AdamCarnage <victorjuniorjack@gmail.com>
2025-02-17 09:20:05 +0300

    removed backend related files from frontend repo

--- server/authentication/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const  AuthController  = require('./controllers/AuthController');
-
-router.post('/register/', AuthController.create_user);
-router.post('/login', AuthController.login_user);
-
-module.exports = router;
--- server/config/token.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    token:{
-        jwt_secret: "612EC3827256EFE8",
-    }
-};
